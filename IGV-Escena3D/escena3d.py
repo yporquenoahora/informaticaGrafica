a1e68b4b5c5a1246bff43e11d297b85ad5d50690
pfhtbqpsvr-ux <pfhtbqpsvr@privaterelay.appleid.com>
2026-05-07 18:34:58 +0200

    Update escena3d.py

--- IGV-Escena3D/escena3d.py
+++ IGV-Escena3D/escena3d.py
@@ -92,6 +92,53 @@ def draw_ground():
     draw.solid_ortho(1, 2, 2 * width_grass, borde_color)
     glPopMatrix()
 
+def vigia_humano():
+    glPushMatrix()
+
+    # Colores humano
+    color_piel = [253 / 255, 209 / 255, 180 / 255]
+    color_armadura = colors.grey_8
+    color_botas = colors.grey_14
+    color_casco = colors.grey_11
+    color_lanza = colors.brown_2
+
+    # --- PIERNAS (2 unidades de ancho cada una) ---
+    glPushMatrix()
+    # Pierna izquierda
+    draw.solid_ortho(2, 6, 2, [color_botas])
+    # Pierna derecha
+    glTranslatef(3, 0, 0)
+    draw.solid_ortho(2, 6, 2, [color_botas])
+    glPopMatrix()
+
+    # --- CUERPO ---
+    glPushMatrix()
+    glTranslatef(-1, 6, -1)  # Centramos un poco respecto a las piernas
+    draw.solid_ortho(7, 9, 4, [color_armadura])
+    glPopMatrix()
+
+    # --- CABEZA Y CASCO ---
+    glPushMatrix()
+    glTranslatef(1, 15, 0)
+    draw.solid_ortho(3, 3, 3, [color_piel])  # Cabeza
+    glTranslatef(-1, 2, -1)
+    draw.solid_ortho(5, 2, 5, [color_casco])  # Casco
+    glPopMatrix()
+
+    # --- BRAZO Y LANZA ---
+    glPushMatrix()
+    glTranslatef(6, 7, 1)
+    draw.solid_ortho(2, 6, 2, [color_armadura])  # Brazo derecho
+    # La Lanza (Palo largo)
+    glTranslatef(0, -3, 0)
+    draw.solid_ortho(1, 22, 1, [color_lanza])
+    # Punta de la lanza
+    glTranslatef(0, 22, 0)
+    draw.solid_ortho(1, 2, 1, [colors.grey_3])
+    glPopMatrix()
+
+    glPopMatrix()
+
 def draw_scene():    
     draw_ground()
 
@@ -134,6 +181,13 @@ def draw_scene():
     glPushMatrix()
     glTranslatef(-120, 0, -115)
     tower()
+    glPopMatrix()
+
+    # Vigia humano
+    glPushMatrix()
+    glTranslatef(10, 5, 10)
+    glScalef(0.8, 0.8, 0.8)
+    vigia_humano()
     glPopMatrix()
     
     # Muralla - 4 tramos independientes
